fde39d87cf6abdf872b52e2c438f3a51272af094
root <root@yq01-sys-hic-k8s-v100-box-a223-0002.yq01.baidu.com>
2021-05-08 15:42:31 +0800

    srn_ex only_pvam

--- ppocr/losses/rec_srn_loss.py
+++ ppocr/losses/rec_srn_loss.py
@@ -28,20 +28,21 @@ class SRNLoss(nn.Layer):
     def forward(self, predicts, batch):
         predict = predicts['predict']
         word_predict = predicts['word_out']
-        gsrm_predict = predicts['gsrm_out']
+        #gsrm_predict = predicts['gsrm_out']
         label = batch[1]
 
         casted_label = paddle.cast(x=label, dtype='int64')
         casted_label = paddle.reshape(x=casted_label, shape=[-1, 1])
 
         cost_word = self.loss_func(word_predict, label=casted_label)
-        cost_gsrm = self.loss_func(gsrm_predict, label=casted_label)
-        cost_vsfd = self.loss_func(predict, label=casted_label)
+        #cost_gsrm = self.loss_func(gsrm_predict, label=casted_label)
+        #cost_vsfd = self.loss_func(predict, label=casted_label)
 
         cost_word = paddle.reshape(x=paddle.sum(cost_word), shape=[1])
-        cost_gsrm = paddle.reshape(x=paddle.sum(cost_gsrm), shape=[1])
-        cost_vsfd = paddle.reshape(x=paddle.sum(cost_vsfd), shape=[1])
+        #cost_gsrm = paddle.reshape(x=paddle.sum(cost_gsrm), shape=[1])
+        #cost_vsfd = paddle.reshape(x=paddle.sum(cost_vsfd), shape=[1])
 
-        sum_cost = cost_word * 3.0 + cost_vsfd + cost_gsrm * 0.15
+        #sum_cost = cost_word * 3.0 + cost_vsfd + cost_gsrm * 0.15
 
-        return {'loss': sum_cost, 'word_loss': cost_word, 'img_loss': cost_vsfd}
+        #return {'loss': sum_cost, 'word_loss': cost_word, 'img_loss': cost_vsfd}
+        return {'loss': cost_word}
